10923deb4425333dc4a81c1fca58ae98d7dc1aa2
Christine Emrich <emrich@meso.net>
2013-11-08 21:33:06 +0100

    Moved Joystick into own module.

--- public/js/terminalControllerSnake/controller.js
+++ public/js/terminalControllerSnake/controller.js
@@ -2,84 +2,21 @@
 Dumb game controller for the snake.
 */
 
-define(function () {
-
-	var SNAP_THRESHOLD = 30;
-	var downMarker = $('#controller .down');
-	var moveMarker = $('#controller .move');
+define(['./joystick'], function (Joystick) {
 
 	function start(session, showSection) {
 
-		var direction = null;
-		var startPos;
+		var joystick = new Joystick(30, onDirectionChange, $('#marker'));
 
 		function onDirectionChange(direction) {
 			$('#marker').attr('class', 'dir' + direction);
 			session.myself.attributes.direction = direction;
 		}
 
-		function getEventPosition(event) {
-			var pos = {};
-			if (event.originalEvent.hasOwnProperty('targetTouches')) {
-				pos.x = event.originalEvent.targetTouches[0].pageX;
-				pos.y = event.originalEvent.targetTouches[0].pageY;
-			} else {
-				pos.x = event.clientX;
-				pos.y = event.clientY;
-			}
-			return pos;
-		}
-
-		function onMove(event) {
-			event.preventDefault();
-			var newDirection = direction;
-			var pos = getEventPosition(event);
-			var snapX = pos.x > startPos.x - SNAP_THRESHOLD &&
-				pos.x < startPos.x + SNAP_THRESHOLD;
-			var snapY = pos.y > startPos.y - SNAP_THRESHOLD &&
-				pos.y < startPos.y + SNAP_THRESHOLD;
-			if (!(snapX && snapY)) {
-				if (snapX) {
-					pos.x = startPos.x;
-					newDirection = (pos.y < startPos.y) ? 0 : 2;
-				}
-				if (snapY) {
-					pos.y = startPos.y;
-					newDirection = (pos.x < startPos.x) ? 3 : 1;
-				}
-				if (newDirection !== direction) {
-					direction = newDirection;
-					onDirectionChange(direction);
-				}
-			}
-			moveMarker.css('top', pos.y);
-			moveMarker.css('left', pos.x);
-		}
-
-		function onUp(event) {
-			$('html').off('touchmove mousemove', onMove);
-			downMarker.hide();
-			moveMarker.fadeOut();
-		}
-
-		// controller starting point
-		// TODO: make this work with multitouch (first touch is king)
-		// TODO: macke this work in firefox mobile
-		// TODO: maybe a own module would be cleaner
-		function onDown(event) {
-			$('html').one('touchend mouseup', onUp);
-			$('html').on('touchmove mousemove', onMove);
-			startPos = getEventPosition(event);
-			$('.marker').css('top', startPos.y);
-			$('.marker').css('left', startPos.x);
-			downMarker.show();
-			moveMarker.fadeIn();
-		}
-
 		function startGame() {
 			showSection('#controller');
 			session.once('finished', onFinished);
-			$('html').on('touchstart mousedown', onDown);
+			joystick.start();
 		}
 
 		function onAgainClick(event) {
@@ -95,7 +32,7 @@ define(function () {
 
 		function onFinished() {
 			// game is finished
-			$('html').off('touchstart mousedown', onDown);
+			joystick.stop();
 			showSection('#controller-finished');
 			$('#controller-finished .again').one('click', onAgainClick);
 			$('#controller-finished .exit').one('click', onExitClick);
---
+++ public/js/terminalControllerSnake/joystick.js
@@ -0,0 +1,90 @@
+/*
+Module for this joystick-like thingy that's part of the controller.
+*/
+
+define(function () {
+
+	function Joystick(threshold, directionCallback, container) {
+
+		var eventEmitter = $('html');
+		var downMarker = container.find('.down');
+		var moveMarker = container.find('.move');
+		var direction = null;
+		var startPos;
+
+		function getEventPosition(event) {
+			var pos = {};
+			if (typeof event.originalEvent.touches !== 'undefined') {
+				pos.x = event.originalEvent.touches[0].pageX;
+				pos.y = event.originalEvent.touches[0].pageY;
+			} else {
+				pos.x = event.clientX;
+				pos.y = event.clientY;
+			}
+			return pos;
+		}
+
+		function onMove(event) {
+			event.preventDefault();
+			var newDirection = direction;
+			var pos = getEventPosition(event);
+			var snapX = pos.x > startPos.x - threshold &&
+				pos.x < startPos.x + threshold;
+			var snapY = pos.y > startPos.y - threshold &&
+				pos.y < startPos.y + threshold;
+			if (!(snapX && snapY)) {
+				if (snapX) {
+					pos.x = startPos.x;
+					newDirection = (pos.y < startPos.y) ? 0 : 2;
+				}
+				if (snapY) {
+					pos.y = startPos.y;
+					newDirection = (pos.x < startPos.x) ? 3 : 1;
+				}
+				if (newDirection !== direction) {
+					direction = newDirection;
+					directionCallback(direction);
+				}
+			}
+			moveMarker.css('top', pos.y);
+			moveMarker.css('left', pos.x);
+		}
+
+		function onUp(event) {
+			eventEmitter.off('touchmove mousemove', onMove);
+			downMarker.hide();
+			moveMarker.fadeOut();
+		}
+
+		// controller starting point
+		// TODO: make this work with multitouch (first touch is king)
+		function onDown(event) {
+			eventEmitter.one('touchend mouseup', onUp);
+			eventEmitter.on('touchmove mousemove', onMove);
+			startPos = getEventPosition(event);
+			downMarker.css('top', startPos.y);
+			downMarker.css('left', startPos.x);
+			moveMarker.css('top', startPos.y);
+			moveMarker.css('left', startPos.x);
+			downMarker.show();
+			moveMarker.fadeIn();
+		}
+
+
+		// PUBLIC
+
+		this.start = function () {
+			eventEmitter.on('touchstart', onDown);
+		};
+
+		this.stop = function () {
+			eventEmitter.off('touchstart mousedown', onDown);
+			downMarker.hide();
+			moveMarker.fadeOut();
+		};
+
+	}
+
+	return Joystick;
+
+});
