ef826fbe97b8e3a8af32a19d48a7fdf4dc3e986c
Christine Emrich <emrich@meso.net>
2013-11-27 19:31:07 +0100

    Documented ScreenArranger

--- multi/shared/screen.js
+++ multi/shared/screen.js
@@ -6,15 +6,49 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 
 /**
- * @module
- * @private
+ * This module contains all classes and utils that are useful
+ * for working with multiple screens. Currently that's only the 
+ * {@link module:shared/screen.ScreenArranger ScreenArranger}.
+ * @module shared/screen
  */
 define(function(require, exports, module) {
 
-	var ScreenArranger = function (session) {
+	/**
+	 * @classdesc This class can be used to arrange multiple clients
+	 * into one big game screen. It does rearrange every time a player
+	 * joins or leaves the given session.<br>
+	 * You can influence the behaviour by using this class as the base
+	 * of your own Arranger class.
+	 * @class
+	 * @param {module:client/session~Session|module:server/session~Session}
+	 *  Session that contains the players that should be arranged into
+	 *  one big screen.
+	 */
+	exports.ScreenArranger = function (session) {
+		/**
+		 * Session that is getting arranged into one big game screen
+		 * @type {module:client/session~Session|module:server/session~Session}
+		 * @readonly
+		 */
 		this.session = session;
+		/**
+		 * Contains one screen object for every player id
+		 * A screen object consists of player, x and y attributes.
+		 * @type {Object.<string, object>}
+		 * @private
+		 */
 		this.screens = {};
+		/**
+		 * total width of the big screen in pixel
+		 * @type {integer}
+		 * @readonly
+		 */
 		this.width = 0;
+		/**
+		 * total height of the big screen in pixel
+		 * @type {integer}
+		 * @readonly
+		 */
 		this.height = 0;
 
 		this.arrange();
@@ -22,14 +56,29 @@ define(function(require, exports, module) {
 		session.on('playerLeft', this.onPlayerLeft.bind(this));
 	};
 
-	ScreenArranger.prototype.localToGlobal = function (player, x, y) {
+	/**
+	 * Converts local pixel coordinates to global ones.
+	 * @param  {module:server/player~Player|module:client/player~Player} player 
+	 * player instance the local coordinates refer to
+	 * @param  {integer} x  local x position in pixel
+	 * @param  {integer} y  local y position in pixel
+	 * @return {object}  { x: globalX, y: globalY }
+	 */
+	exports.ScreenArranger.prototype.localToGlobal = function (player, x, y) {
 		var screen = this.screens[player.id];
 		var globalX = screen.left + x;
 		var globalY = screen.top + y;
 		return {x: globalX, y: globalY };
 	};
 
-	ScreenArranger.prototype.globalToLocal = function (x, y) {
+	/**
+	 * Converts global pixel coordinates into their corresponding
+	 * local ones.
+	 * @param  {integer} x  global x position in pixel
+	 * @param  {integer} y  global y position in pixel
+	 * @return {object}  { player: localPlayer, x: localX, y: localY }
+	 */
+	exports.ScreenArranger.prototype.globalToLocal = function (x, y) {
 		var player = this.getPlayerAtCoords(x, y);
 		var screen = this.screens[player.id];
 		var localX = x - screen.left;
@@ -37,18 +86,41 @@ define(function(require, exports, module) {
 		return {player: player, x: localX, y: localY };
 	};
 
-	ScreenArranger.prototype.isScreenHit = function (screen, x, y) {
+	/**
+	 * @param  {object}  screen  any screen object that should be tested
+	 * @param  {integer}  x  global x position in pixel
+	 * @param  {integer}  y  global y position in pixel
+	 * @return {boolean}  true if the given global coordinates lie inside 
+	 * the given screen object, else false
+	 * @private
+	 */
+	exports.ScreenArranger.prototype.isScreenHit = function (screen, x, y) {
 		return x >= screen.left &&
 			x < screen.left + screen.player.width &&
 			y >= screen.top &&
 			y < screen.top + screen.player.height;
 	};
 
-	ScreenArranger.prototype.isPlayerHit = function (player, x, y) {
+	/**
+	 * @param  {module:server/player~Player|module:client/player~Player} player 
+	 * any player object connected to the arranged session
+	 * @param  {integer}  x  global x position in pixel
+	 * @param  {integer}  y  global y position in pixel
+	 * @return {boolean}  true if the given coordinates lie within
+	 * the screen of the given player
+	 */
+	exports.ScreenArranger.prototype.isPlayerHit = function (player, x, y) {
 		return this.isScreenHit(this.screens[player.id], x, y);
 	};
 
-	ScreenArranger.prototype.getPlayerAtCoords = function (x, y) {
+	/**
+	 * @param  {integer}  x  global x position in pixel
+	 * @param  {integer}  y  global y position in pixel
+	 * @return {module:server/player~Player|module:client/player~Player}
+	 * player object whose screen lies beneath the given coordinates
+	 * or null when no player can be found at this position
+	 */
+	exports.ScreenArranger.prototype.getPlayerAtCoords = function (x, y) {
 		for (var i in this.screens) {
 			var screen = this.screens[i];
 			if (this.isScreenHit(screen, x, y)) {
@@ -58,7 +130,13 @@ define(function(require, exports, module) {
 		return null;
 	};
 
-	ScreenArranger.prototype.arrange = function () {
+	/**
+	 * Rearranges the screens of all players currently connected to
+	 * the arranged session. Width, height and other attributes
+	 * will be recalculated and may change.
+	 * @private
+	 */
+	exports.ScreenArranger.prototype.arrange = function () {
 		var screens = {};
 		var height = 0;
 		var xPos = 0;
@@ -77,15 +155,14 @@ define(function(require, exports, module) {
 		this.height = height;
 	};
 
-	ScreenArranger.prototype.onPlayerJoined = function (event) {
+	exports.ScreenArranger.prototype.onPlayerJoined = function (event) {
 		this.arrange();
 	};
 
-	ScreenArranger.prototype.onPlayerLeft = function (event) {
+	exports.ScreenArranger.prototype.onPlayerLeft = function (event) {
 		this.arrange();
 	};
 
-	exports.ScreenArranger = ScreenArranger;
 	return exports;
 
  });
