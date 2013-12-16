6fd61522ccb13e48471bb7b7f69c570456bfd729
Christine Emrich <christine.emrich@web.de>
2013-12-16 14:56:25 +0100

    started to adapt serverOnescreenSnake.js to new screen arranger

--- games/serverOnescreenSnake.js
+++ games/serverOnescreenSnake.js
@@ -30,11 +30,11 @@ exports.Game = function (session) {
 		var localY = Math.round(display.height / 2);
 
 		this.speed = 5;
-		this.pos = arranger.localToGlobal(display, localX, localY);
-		this.lastPos = null;
 		this.dir = -1;
 		this.lastDir = -1;
-		this.curSegment = new Segment(this.pos.x, this.pos.y);
+
+		var initialPos = display.screen.localToGlobal(localX, localY);
+		this.curSegment = new Segment(initialPos.x, initialPos.y);
 		this.segments = [this.curSegment];
 
 		this.update = function () {
@@ -44,41 +44,40 @@ exports.Game = function (session) {
 
 		this.move = function () {
 			this.lastDir = this.dir;
-			this.lastPos = this.pos;
 			this.dir = owner.attributes.direction || 0;
-			switch (this.dir) {
-			case 0:
-				this.pos = arranger.getUp(this.pos.x, this.pos.y, this.speed);
-				break;
-			case 1:
-				this.pos = arranger.getRight(this.pos.x, this.pos.y, this.speed);
-				break;
-			case 2:
-				this.pos = arranger.getDown(this.pos.x, this.pos.y, this.speed);
-				break;
-			case 3:
-				this.pos = arranger.getLeft(this.pos.x, this.pos.y, this.speed);
-				break;
-			}
 
 			if (this.dir !== this.lastDir) {
 				// direction changed - start a new segment
-				this.curSegment = new Segment(this.lastPos.x, this.lastPos.y);
+				this.curSegment = new Segment(this.curSegment.x, this.curSegment.y);
 				this.segments.push(this.curSegment);
 			}
-			// TODO: local screen boundary - create a new segment
 
-			// update current segment
-			this.curSegment.x = this.pos.x;
-			this.curSegment.y = this.pos.y;
+			switch (this.dir) {
+			case 0:
+				this.curSegment.y -= this.speed;
+				break;
+			case 1:
+				this.curSegment.x += this.speed;
+				break;
+			case 2:
+				this.curSegment.y += this.speed;
+				break;
+			case 3:
+				this.curSegment.x -= this.speed;
+				break;
+			}
+		};
+
+		this.isAlive = function () {
+			return arranger.getPlayerAtCoords(this.curSegment.x, this.curSegment.y)
+				!== null;
 		};
 
 		this.updateDisplay = function () {
 			var width = this.curSegment.getWidth();
 			var height = this.curSegment.getHeight();
-			var locals = arranger.globalToLocals(this.curSegment.startX,
+			var locals = arranger.globalRectToLocals(this.curSegment.startX,
 				this.curSegment.startY, width, height);
-			var local;
 			for (var i in locals) {
 				local = locals[i];
 				this.owner.message('draw',
@@ -94,8 +93,16 @@ exports.Game = function (session) {
 	}
 
 	function move() {
+		var dead = [];
 		snakes.forEach(function (snake) {
 			snake.update();
+			if (!snake.isAlive()) {
+				console.log('dead:', snake);
+				dead.push(snake);
+			}
+		});
+		dead.forEach(function (snake) {
+			snakes.splice(snakes.indexOf(snake));
 		});
 	}
 
