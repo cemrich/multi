0c58acc04479b85ae319d3ed5ea4b3662bf012b4
Christine Emrich <christine.emrich@web.de>
2014-01-08 12:18:35 +0100

    Moved Tron game to its own class for separation of concern

---
+++ public/js/serverOnescreenSnake/Game.js
@@ -0,0 +1,98 @@
+define(function (require, exports, module) {
+
+	var Screen = require('./Screen');
+	var multiModule = require('../lib/multi');
+	var Joystick = require('../lib/joystick');
+
+
+	var Game = function(session, onError, showSection) {
+
+		var game = this;
+		this.onError = onError;
+		this.showSection = showSection;
+		this.arranger = new multiModule.screens.HorizontalArranger(session);
+		this.screen = new Screen(session, this.arranger);
+		this.joystick = new Joystick(30, function (direction) {
+				session.myself.attributes.direction = direction;
+			}, false, $('.joystick'), $('html'));
+		
+
+		// VIEW SETUP
+		this.screen.updateBorders();
+		this.showSection('joined');
+		$('#status').text('connected');
+		$('.join-url').text(session.joinSessionUrl);
+		$('.join-url').attr('href', 'http://' + session.joinSessionUrl);
+
+
+		// LISTENERS
+		session.getPlayerArray().forEach(this.addPlayer.bind(this));
+
+		session.on('playerJoined', function (event) {
+			game.addPlayer(event.player);
+		});
+		session.on('startGame', this.onStartGame.bind(this));
+		session.on('finished', this.onGameFinished.bind(this));
+
+		$('button.start').click(function () {
+			session.message('startGame');
+		});
+		this.arranger.on('arrangementChanged', function () {
+			game.screen.updateBorders();
+		});
+
+		session.myself.on('died', this.onPlayerDied.bind(this));
+		session.myself.getAttributeAsync('color')
+			.then(this.setPlayerColor.bind(this));
+	};
+
+	Game.prototype.setPlayerColor = function (color) {
+		$('.joystick > *').css('background-color', color);
+		$('html').css('color', color);
+		$('button').css('border-color', color);
+	};
+
+	Game.prototype.onStartGame = function () {
+		this.screen.drawStartingPoint();
+		this.showSection('game');
+		this.joystick.start();
+	};
+
+	Game.prototype.onGameFinished = function () {
+		this.showSection('joined');
+		this.screen.clearPlayers();
+		this.joystick.stop();
+	};
+
+	Game.prototype.onPlayerDied = function () {
+		this.joystick.stop();
+	};
+
+	Game.prototype.addPlayer = function (player) {
+		var game = this;
+		var playerView = $('<div></div>');
+		playerView.addClass('player');
+		playerView.css('height', player.height/10);
+		playerView.css('max-width', player.width/10);
+		playerView[0].style.order = player.number;
+		$('.players').append(playerView);
+
+		player.getAttributeAsync('color').then(function (color) {
+			playerView.css('background-color', color);
+		});
+		player.on('draw', function (event) {
+			var data = event.data;
+			game.screen.drawPlayer(player, data.x, data.y, data.width, data.height);
+		});
+		player.on('disconnected', function () {
+			playerView.remove();
+		});
+	};
+
+	Game.prototype.stop = function () {
+		this.onError('session has been destroyed');
+		this.joystick.stop();
+	};
+
+	return Game;
+});
--- public/js/serverOnescreenSnake/index.js
+++ public/js/serverOnescreenSnake/index.js
@@ -4,13 +4,8 @@ requirejs.config({
 	}
 });
 
-requirejs(['./Screen', '../lib/multi', '../lib/joystick', '../SERVER', '../lib/jquery-2.0.0.min'],
-	function (Screen, multiModule, Joystick, SERVER) {
-
-	var screen = null;
-	var session = null;
-	var arranger = null;
-	var joystick = null;
+requirejs(['../lib/multi', './Game', '../SERVER', '../lib/jquery-2.0.0.min'],
+	function (multiModule, Game, SERVER) {
 
 	var multiOptions = {
 		server: SERVER,
@@ -24,100 +19,22 @@ requirejs(['./Screen', '../lib/multi', '../lib/joystick', '../SERVER', '../lib/j
 		$('#' + section).show();
 	}
 
-	function addPlayer(player) {
-		var playerView = $('<div></div>');
-		playerView.addClass('player');
-		playerView.css('height', player.height/10);
-		playerView.css('max-width', player.width/10);
-		playerView[0].style.order = player.number;
-		$('.players').append(playerView);
-
-		player.getAttributeAsync('color').then(function (color) {
-			playerView.css('background-color', color);
-		});
-		player.on('draw', function (event) {
-			var data = event.data;
-			screen.drawPlayer(player, data.x, data.y, data.width, data.height);
-		});
-		player.on('disconnected', function () {
-			playerView.remove();
-		});
-	}
-
 	function onError(message) {
 		showSection('error');
 		$('#status').text('disconnected');
 		$('#error').text(message);
 	}
 
-	function onStartGame() {
-		screen.drawStartingPoint();
-		showSection('game');
-		joystick.start();
-	}
-
-	function onGameFinished() {
-		showSection('joined');
-		screen.clearPlayers();
-		joystick.stop();
-	}
-
-	function onDied() {
-		joystick.stop();
-	}
-
-	function onSession(s) {
-		session = s;
-
-		function onDirectionChange(direction) {
-			session.myself.attributes.direction = direction;
-		}
-
-		arranger = new multiModule.screens.HorizontalArranger(session);
-		screen = new Screen(session, arranger);
-		joystick = new Joystick(30, onDirectionChange, false, $('.joystick'), $('html'));
-		
-		screen.updateBorders();
-		showSection('joined');
-		$('#status').text('connected');
-		$('.join-url').text(session.joinSessionUrl);
-		$('.join-url').attr('href', 'http://' + session.joinSessionUrl);
-
-		session.getPlayerArray().forEach(addPlayer);
-
-		session.on('destroyed', onSessionDestroyed);
-		session.on('playerJoined', function (event) {
-			addPlayer(event.player);
+	function onSession(session) {
+		var game = new Game(session, onError, showSection);
+		session.on('destroyed', function () {
+			game.stop();
 		});
-		session.on('startGame', onStartGame);
-		session.on('finished', onGameFinished);
-
-		$('button.start').click(function () {
-			session.message('startGame');
-		});
-		arranger.on('arrangementChanged', function () {
-			screen.updateBorders();
-		});
-
-		session.myself.on('died', onDied);
-		session.myself.getAttributeAsync('color').then(function (color) {
-			$('.joystick > *').css('background-color', color);
-			$('html').css('color', color);
-			$('button').css('border-color', color);
-		});
-	}
-
-	function onSessionDestroyed() {
-		onError('session has been destroyed');
-		if (joystick !== null) {
-			joystick.stop();
-		}
 	}
 
 	function onSessionFailed(error) {
 		onError(error.message);
 	}
-
 
 	var multi = multiModule.init(multiOptions);
 	multi.autoJoinElseCreateSession().then(onSession, onSessionFailed).done();
